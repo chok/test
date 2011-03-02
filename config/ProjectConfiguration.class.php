4e532c7bdca9018f3bbcc3ab0dbc44db7edfddf5
Maxime Picaud <maxime.picaud@agence-shape.fr>
2011-03-02 17:55:49 +0100

    Initial commit

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'C://Users//kiki//work//symfony//1.4-git//lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
